59b122da47487394b5cc2c5afcf34e5f5864c41b
Felipemart123 <felipemart1876@gmail.com>
2022-08-04 14:04:59 -0400

    taco parse

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -23,9 +23,11 @@
             }
 
             // grab the latitude from your array at index 0
+            var lat = double.Parse(cells[0]);
             // grab the longitude from your array at index 1
+            var lon = double.Parse(cells[1]);
             // grab the name from your array at index 2
-
+            var name = cells[2];
             // Your going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
 
@@ -34,7 +36,8 @@
 
             // Then, you'll need an instance of the TacoBell class
             // With the name and point set correctly
-
+            var tacobell = new TacoBell();
+            _ = tacobell.Location;
             // Then, return the instance of your TacoBell class
             // Since it conforms to ITrackable
 
